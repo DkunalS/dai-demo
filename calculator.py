a990a44065be36ae81f787b761f2166025e2a00e
dai <dai@students.acts.in>
2024-12-19 11:54:41 +0530

    added division functionality

--- calculator.py
+++ calculator.py
@@ -4,10 +4,14 @@ def add(num1, num2):
 def sub(num1, num2):
     return num1 - num2
 
-def mul(nu1, num2):
+def mul(num1, num2):
     return num1 * num2
 
+def div(num1, num2):
+    return num1/num2
 
 print(add(3, 2))
 print(sub(8, 4))
 print(mul(7, 5))
+print(div(8, 2))
+
