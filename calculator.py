d6f9ea9be89bb69a873eb8ae0a82e7a143f7864a
dai <dai@students.acts.in>
2024-12-19 12:09:43 +0530

    Revert "added caaling the function for mul"
    This reverts commit f8b2fb2513c3458d842134b02e7be0c204ae268e.

--- calculator.py
+++ calculator.py
@@ -11,7 +11,7 @@ def div(num1, num2):
     return num1/num2
 
 print(add(3, 2))
-print(sub(8, 4))
-print(mul(7, 5))
-print(div(8, 2))
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of f8b2fb2 (added caaling the function for mul)
